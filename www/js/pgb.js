04fb5d7e6486cf0f68e9777c62f5d6c8800c1ee9
Tadeuk S <ted.slawik@gmail.com>
2018-03-24 14:23:27 +0100

    Add new button

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -20,3 +20,16 @@ function deviceInfo() {
 	navigator.notification.alert(info);
 	
 }
+function deviceInfo() {
+
+	info =  'Tadeusz Slawik' + '\n' +
+			'=====' + '\n' +
+			'Device Name    : '     + device.name     + '\n' + 
+			'Device Cordova : '  + device.cordova + '\n' + 
+			'Device Platform: ' + device.platform + '\n' + 
+			'Device UUID    : '     + device.uuid     + '\n' + 
+			'Device Model   : '    + device.model     + '\n' + 
+			'Device Version : '  + device.version  + '\n';
+
+	navigator.notification.alert(info);
+}
